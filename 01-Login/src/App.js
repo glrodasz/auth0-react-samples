622a5aaf1a0b36041ca16e4239e8aadf6e20e52e
Guillermo Rodas <garethderioth@gmail.com>
2019-04-11 07:02:00 -0500

    Delete deprecated examples add react-router-dom

--- 01-Login/src/App.js
+++ 01-Login/src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from "react";
 
-// import "jquery";
-// import "samples-bootstrap-theme";
 import "samples-bootstrap-theme/dist/css/auth0-theme.css";
-
-// import logo from "./logo.svg";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-import initFontAwesome from "./utils/initFontAwesome";
 
 import { Container } from "reactstrap";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 
+import initFontAwesome from "./utils/initFontAwesome";
 initFontAwesome();
 
 class App extends Component {
