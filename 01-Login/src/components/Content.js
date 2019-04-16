c13396a6659c9bdbf5f9824b676d3bbfdf42f5b8
Guillermo Rodas <garethderioth@gmail.com>
2019-04-16 11:47:24 -0500

    Fix content to use flexbox

--- 01-Login/src/components/Content.js
+++ 01-Login/src/components/Content.js
@@ -5,16 +5,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import contentData from "../utils/contentData";
 
-// FIXME: Use flexbox instead of Row/Col
 class Content extends Component {
   render() {
     return (
       <div className="next-steps">
         <h2 className="mt-5 text-center">What can I do next?</h2>
-        {contentData.map((row, i) => (
-          <Row key={i} className="d-flex justify-content-between">
-            {row.map((col, j) => (
-              <Col key={j} md={5}>
+          <Row className="d-flex justify-content-between">
+            {contentData.map((col, i) => (
+              <Col key={i} md={5}>
                 <h6 className="mb-3">
                   <a href={col.link}>
                     <FontAwesomeIcon icon="link" />
@@ -25,7 +23,6 @@ class Content extends Component {
               </Col>
             ))}
           </Row>
-        ))}
       </div>
     );
   }
